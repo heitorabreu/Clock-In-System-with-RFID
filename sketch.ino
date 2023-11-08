7a9da4d821e909b635c41e1f3f31d7e88ea4fe11
heitorabreu <96392593+heitorabreu@users.noreply.github.com>
2023-11-08 19:31:40 -0300

    Add treatment to different tags

--- sketch.ino
+++ sketch.ino
@@ -1,4 +1,3 @@
-//Biblioteca mfrc522
 //exemplo dumpinfo
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
@@ -6,16 +5,27 @@
 
 #define SS_PIN 9
 #define RST_PIN 10
+//**************************************
+//Substituir endereço lcd para 0x3F
 LiquidCrystal_I2C lcd(0x27,16,2);
-time_t t;
 MFRC522 mfrc522(SS_PIN, RST_PIN); //Criar instância MFRC522
 
+typedef struct tag{
+  byte UID[4];
+  bool pass;
+  int hora, minuto, segundo;
+}TAG;
 
-//Declarar aqui um byte accessUID[4] com o endereço da tag rfid
-byte accessUID[4] = {00, 00, 00, 00};
-int concedeAcesso();
-bool pass;
-int hora, minuto, segundo;
+time_t t;
+int selecionaTAG();
+int i;
+
+//DECLARAR OS CARTÕES AQUI
+TAG tag[2];
+tag[0].UID = {00,00,00,00};
+tag[0].pass = false;
+tag[1].UID = {11,11,11,11};
+tag[1].pass = false;
 
 void setup() {
   Serial.begin(9600);
@@ -26,31 +36,21 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Aproxime a tag");
+  lcd.print("Aproxime a tag.");
   t = now();
   lcd.setCursor(0, 1);
   lcd.print(hour(t));
   lcd.print(":");
   lcd.print(minute(t));
-  Serial.println("Escaneie o PICC para ver UID, SAK, tipo e blocos de dados");
 }
 
 void loop() {
-  t = now();
-  hora = hour(t);
-  minuto = minute(t);
-  segundo = second(t);
-  int aux = hour(t)*60 + minute(t);
-  aux = aux - (hora*60 + minuto);
-  lcd.clear();
-  lcd.print(segundo);
-  lcd.print(":");
-  lcd.print(minuto);
-  lcd.print(":");
-  lcd.print(hora);
-  
   //Reseta o loop se nenhum cartão está presente no sensor
   if( ! mfrc522.PICC_IsNewCardPresent()){
+    lcd.setCursor(0, 1);
+    lcd.print(hour(t));
+    lcd.print(":");
+    lcd.print(minute(t));
     return;
   }
 
@@ -58,54 +58,81 @@ void loop() {
   if( ! mfrc522.PICC_ReadCardSerial()){
     return;
   }
-
-  if(concedeAcesso()){
-    if(pass == false){
-      t = now();
-      hora = hour(t);
-      minuto = minute(t);
-      segundo = second(t);
-      lcd.print("Acesso concedido às ");
-      //lcd.clear();
-      lcd.print(hora);
-      lcd.print(":");
-      lcd.print(minuto);
-      lcd.print(":");
-      lcd.print(segundo);
-      pass = true;
-    }
-    else{
-      t = now();
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tempo decorrido: ");
-      lcd.setCursor(0, 1);
-
-      //procedimento para escrever a hora da maneira correta
-      int aux = hour(t)*3600 + minute(t)*60 + second(t);
-      aux = aux - (hora*3600 + minuto*60 + segundo);
-      lcd.print(aux/3600);
-      aux -= (aux/3600)*3600;
-      lcd.print(":");
-      lcd.print(aux/60);
-      aux -= (aux/60)*60;
-      lcd.print(":");
-      lcd.print(aux);
-
-      pass = false;
-    }
-  }
-  else {
+  
+  //Procedimento para identificar qual cartão está sendo passado no leitor
+  i = selecionaTAG();
+  if(i == -1){
     lcd.print("Acesso negado.");
+    return;
   }
 
+  if(tag[i].pass == false){
+    t = now();
+    tag[i].hora = hour(t);
+    tag[i].minuto = minute(t);
+    tag[i].segundo = second(t);
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("ID: ");
+    lcd.print(tag[i].UID[0]);
+    lcd.print(tag[i].UID[1]);
+    lcd.print(tag[i].UID[2]);
+    lcd.print(tag[i].UID[3]);
+    
+    lcd.setCursor(0, 1);
+    lcd.print("HORA:");
+    lcd.print(tag[i].hora);
+    lcd.print(":");
+    lcd.print(tag[i].minuto);
+    lcd.print(":");
+    lcd.print(tag[i].segundo);
+    
+    tag[i].pass = true;
+    delay(2000);
+  }
+  else{
+    t = now();
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("ID:");
+    lcd.print(tag[i].UID[0]);
+    lcd.print(tag[i].UID[1]);
+    lcd.print(tag[i].UID[2]);
+    lcd.print(tag[i].UID[3]);
+    
+    lcd.setCursor(0, 1);
+    lcd.print("TEMPO:");
+    //procedimento para escrever a hora da maneira correta (HORA:MINUTO:SEGUNDO)
+    int aux = hour(t)*3600 + minute(t)*60 + second(t);
+    aux = aux - (tag[i].hora*3600 + tag[i].minuto*60 + tag[i].segundo);
+    
+    lcd.print(aux/3600);
+    aux -= (aux/3600)*3600;
+    lcd.print(":");
+    lcd.print(aux/60);
+    aux -= (aux/60)*60;
+    lcd.print(":");
+    lcd.print(aux);
+
+    tag[i].pass = false;
+    delay(2000);
+  }
 }
 
-int concedeAcesso(){
-  int i;
-  for(i=0; i<4; i++){
-    if(mfrc522.uid.uidByte[i] != accessUID[i])
-      return 0;
-  }
-  return 1;
+//Escolhe qual TAG está no leitor
+int selecionaTAG(){
+  if(mfrc522.uid.uidByte[0] == tag[0].UID[0] 
+  && mfrc522.uid.uidByte[1] == tag[0].UID[1] 
+  && mfrc522.uid.uidByte[2] == tag[0].UID[2] 
+  && mfrc522.uid.uidByte[3] == tag[0].UID[3] &&)
+    return 0;
+  else if(mfrc522.uid.uidByte[0] == tag[1].UID[0] 
+  && mfrc522.uid.uidByte[1] == tag[1].UID[1] 
+  && mfrc522.uid.uidByte[2] == tag[1].UID[2] 
+  && mfrc522.uid.uidByte[3] == tag[1].UID[3] &&)
+    return 1;
+  else
+    return -1;
 }
